c88062c7ec2c6d1ace2269a9a4463de5df1c84cc
gaodunqiao <gaodq7c7+github@gmail.com>
2017-07-14 18:33:35 +0800

    Update Makefile

---
+++ floyd/include/version.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-present, Qihoo, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+// Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+#pragma once
+
+#define FLOYD_MAJOR 1
+#define FLOYD_MINOR 3
+#define FLOYD_PATCH 1
---
+++ floyd/src.mk
@@ -0,0 +1,21 @@
+LIB_SOURCES = \
+	src/build_version.cc \
+	src/floyd_apply.cc \
+	src/floyd_client_pool.cc \
+	src/floyd_command.cc \
+	src/floyd_context.cc \
+	src/floyd_impl.cc \
+	src/floyd_options.cc \
+	src/floyd.pb.cc \
+	src/floyd_peer_thread.cc \
+	src/floyd_primary_thread.cc \
+	src/floyd_worker.cc \
+	src/logger.cc \
+	src/raft_log.cc \
+
+# EXAMPLE_SOURCE = \
+# 	examples/binlog_example.cc \
+# 	examples/cond_lock_example.cc \
+# 	examples/conf_example.cc \
+# 	examples/hash_example.cc \
+# 	examples/mutex_example.cc \
